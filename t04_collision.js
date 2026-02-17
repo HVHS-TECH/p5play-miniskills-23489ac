6402a418a651928816c2ff1be72097001548627b
23489ac <23489ac@hvhs.school.nz>
2026-02-17 02:12:56 +0000

    to5 done

--- t04_collision.js
+++ t04_collision.js
@@ -17,8 +17,14 @@ mole.rotationSpeed = 2;
 mole.vel.x = 2;
 
 platform_1 = new Sprite(79, 280, 290, 10, 'a');
+platform_1.rotationSpeed = -1;
+platform_1.bounciness = 0 
+platform_1.friction = 0 
+platform_1.drag = 0
 platform_2 = new Sprite(700, 280, 290, 10, 't');
+platform_2.rotationSpeed = 0.5;
 platform_3 = new Sprite(479, 380, 290, 10, 'r');
+platform_3.rotationSpeed = 0.5;
 	console.log("setup: ");
 
 }
