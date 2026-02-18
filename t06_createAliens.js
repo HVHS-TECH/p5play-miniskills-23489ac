012ddaceae6c03ed57c43e3a3c56e3c6a1d4f2ed
23489ac <23489ac@hvhs.school.nz>
2026-02-18 22:00:44 +0000

    on to7

--- t06_createAliens.js
+++ t06_createAliens.js
@@ -1,6 +1,6 @@
 /*******************************************************/
 // P5.play: t06_createAliens
-// Create aliens on mouse click
+// Create walls around the canvas
 // Written by ???
 /*******************************************************/
 	
@@ -8,7 +8,35 @@
 // setup()
 /*******************************************************/
 function setup() {
-	console.log("setup: ");
+	cnv = new Canvas (700, 750);
+	wallLH  = new Sprite(350, height/0.673, 800, height, 'k');
+	wallLH.color = 'black';
+	wallRH  = new Sprite(5, 5, 2000, 15, 'k');
+	wallTop = new Sprite(5, 9, 15, 2000, 'k');
+	wallBot = new Sprite(695,30, 15, 2000,'k');
+
+
+	ball_1 = new Sprite(width/8, height/4, 80, 'd');
+	ball_1.color = 'green';
+	ball_1.vel.x = 2;
+	ball_1.bounciness = 1;
+	ball_1.friction = 0;
+	ball_1.drag = 0;
+	
+	cnv = new Canvas (700, 750);
+	wallLH  = new Sprite(750, height/0.673, 800, height, 'k')
+	ball_1 = new Sprite(width/8, height/4, 80, 'd');
+
+	for ( i = 0; i <80; i++) {
+		alien = new Sprite(7,9);
+		alien.vel.x = 3;
+		alien.vel.y = 4; 
+		alien.bounciness = 1;
+		alien.friction = 0;
+
+}
+
+console.log("setup: ");
 
 }
 	
@@ -16,7 +44,8 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('skyblue'); 
+	background('skyblue'); 
 }
 
 /*******************************************************/
